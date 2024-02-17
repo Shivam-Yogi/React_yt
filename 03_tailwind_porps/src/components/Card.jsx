79b437834dc603bdafd98b145aef0b4a98186d6b
Shivam_yogi <shivamyogi99@gmail.com>
2024-02-17 17:26:49 +0530

    interview points

---
+++ 03_tailwind_porps/src/components/Card.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Card(props) {
+    console.log("props", props)
+    return (
+        <div className="relative h-[400px] w-[300px] rounded-md">
+            <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+  <img className="w-24 h-24 md:rou rounded-full mx-auto" src="https://images.pexels.com/photos/17604370/pexels-photo-17604370/free-photo-of-beautiful-woman-sitting-under-a-tree.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
+  <div className="pt-6 md:p-8 text-center space-y-4">
+    <blockquote>
+      <p className="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+    <figcaption className="font-medium">
+      <div className="text-sky-500 dark:text-sky-400">
+        Sarah Dayan
+      </div>
+      <div className="text-slate-700 dark:text-slate-500">
+        Staff Engineer, Algolia
+      </div>
+    </figcaption>
+  </div>
+</figure>
+        </div>
+    )
+}
+
+export default Card
